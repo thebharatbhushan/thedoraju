0c69c53b0bc98f9b0b5d101b556524459ebe88d9
Bharat Bhushan <67146904+thebharatbhushan@users.noreply.github.com>
2023-07-11 15:52:34 +0530

    Add files via upload

--- src/gamestart.js
+++ src/gamestart.js
@@ -1,42 +1,36 @@
-	
 export default class Scene2 extends Phaser.Scene {
-    constructor() {
-      super('gamenew')
-    }
-  
-    preload(){
-    	this.load.image('skyBG', './src/assets/skybg.jpg');
-    	this.load.image('player', './src/assets/doraemon.png');
-    }
-    create() {
-    	this.add.image(960, 540, 'skyBG');
-   	
-    	this.cursors = this.input.keyboard.createCursorKeys();
-
-        this.player = this.physics.add.image(400, 300, 'player');
-
-        this.player.setCollideWorldBounds(true);
-	      
-	    }
-update(){
-	this.player.setVelocity(0);
-
-        if (this.cursors.left.isDown)
-        {
-            this.player.setVelocityX(-300);
-        }
-        else if (this.cursors.right.isDown)
-        {
-            this.player.setVelocityX(300);
-        }
-
-        if (this.cursors.up.isDown)
-        {
-            this.player.setVelocityY(-300);
-        }
-        else if (this.cursors.down.isDown)
-        {
-            this.player.setVelocityY(300);
-        }
-}	    
+  constructor() {
+    super("gamenew");
   }
+
+  preload() {
+    this.load.image("skyBG", "./src/assets/skybg.jpg");
+    this.load.image("player", "./src/assets/doraemon.png");
+  }
+  create() {
+    this.cameras.main.setBounds(0, 0, 4740, 0);
+    this.physics.world.setBounds(0, 0, 4740, 1000);
+
+    this.add.image(960, 540, "skyBG");
+    this.add.image(2850, 540, "skyBG");
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.player = this.physics.add.image(400, 300, "player");
+    this.player.setCollideWorldBounds(true);
+    this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
+  }
+  update() {
+    this.player.setVelocity(0);
+
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-500);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(500);
+    }
+
+    if (this.cursors.up.isDown) {
+      this.player.setVelocityY(-500);
+    } else if (this.cursors.down.isDown) {
+      this.player.setVelocityY(500);
+    }
+  }
+}
